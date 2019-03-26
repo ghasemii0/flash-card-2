1743be25db73b84ee1ef3e71e76d6e9876eb6554
Mustafa Ghasemi and Nathan Aden <nathan@>
2019-03-26 12:01:11 -0700

    creat all initial file, add scripts for buttons,

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $(".btn").click(function() {
+    $(".discription").hide();
+      var sentid =  event.target.id;
+      var discrip = "#" + sentid.replace("btn", "txt")
+      $(discrip).fadeIn();
+      $(".btn").removeClass("btn-dark");
+      $("#" + sentid).addClass("btn-dark");
+
+  });
+});
