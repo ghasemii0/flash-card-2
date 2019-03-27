9a6044ed33a29f7d558b061eb223dd0d9be2c1f0
Mustafa <mustafa@Mustafas-MacBook-Pro.local>
2019-03-26 20:00:13 -0700

    scritps codes follow which we learn today

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,91 @@
 $(document).ready(function() {
-
-  $(".btn").click(function() {
+  $("button#btn-ja").click(function(){
     $(".discription").hide();
-      var sentid =  event.target.id;
-      var discrip = "#" + sentid.replace("btn", "txt")
-      $(discrip).fadeIn();
-      $(".btn").removeClass("btn-dark");
-      $("#" + sentid).addClass("btn-dark");
+    $("#txt-ja").show();
+  });
+    $("button#btn-op").click(function(){
+      $(".discription").hide();
+      $("#txt-op").show();
+  });
+    $("button#btn-va").click(function(){
+      $(".discription").hide();
+      $("#txt-va").show();
+  });
+    $("button#btn-na").click(function(){
+      $(".discription").hide();
+      $("#txt-na").show();
+  });
+    $("button#btn-me").click(function(){
+      $(".discription").hide();
+      $("#txt-me").show();
+});
+    $("button#btn-ar").click(function(){
+      $(".discription").hide();
+      $("#txt-ar").show();
+  });
+    $("button#btn-pa").click(function(){
+      $(".discription").hide();
+      $("#txt-pa").show();
+});
+  $(".btn-pa").click(function(){
+    $(".discription").hide();
+    $("#txt-pa").show();
+  });
+    $("button#btn-re").click(function(){
+      $(".discription").hide();
+      $("#txt-re").show();
+  });
+    $("button#btn-ch").click(function(){
+      $(".discription").hide();
+      $("#txt-ch").show();
+  });
+    $("button#btn-st").click(function(){
+      $(".discription").hide();
+      $("#txt-st").show();
+  });
+    $("button#btn-bo").click(function(){
+      $(".discription").hide();
+      $("#txt-bo").show();
+  });
+  $("button#btn-un").click(function(){
+    $(".discription").hide();
+    $("#txt-un").show();
+  });
+    $("button#btn-nan").click(function(){
+      $(".discription").hide();
+      $("#txt-nan").show();
+  });
+    $("button#btn-es").click(function(){
+      $(".discription").hide();
+      $("#txt-es").show();
+  });
+    $("button#btn-al").click(function(){
+      $(".discription").hide();
+      $("#txt-al").show();
+  });
+    $("button#btn-co").click(function(){
+      $(".discription").hide();
+      $("#txt-co").show();
+  });
+  $("button#btn-jq").click(function(){
+    $(".discription").hide();
+    $("#txt-jq").show();
+  });
+    $("button#btn-at").click(function(){
+      $(".discription").hide();
+      $("#txt-at").show();
   });
 });
+
+
+
+
+
+// $(".btn").click(function() {
+//   $(".discription").hide();
+//   var sentid =  event.target.id;
+//   var discrip = "#" + sentid.replace("btn", "txt")
+//   $(discrip).fadeIn();
+//   $(".btn").removeClass("btn-dark");
+//   $("#" + sentid).addClass("btn-dark");
+// });
